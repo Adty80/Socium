61d7838389a04017ea911834717598007c982c29
Aditya Sawarkar <Adityasawarkar31@gmail.com>
2024-09-02 14:31:46 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,10 @@
 const nextConfig = {};
 
 export default nextConfig;
+// next.config.js
+module.exports = {
+    basePath: '/Socium', // Change this to your GitHub repo name
+    assetPrefix: '/Socium/', // Change this to your GitHub repo name
+    // Other Next.js configurations
+  };
+  
